fc5684ead853bd3df9eb660d18519123bcb1f2b7
yasinhessnawi1 <79910859+yasinhessnawi1@users.noreply.github.com>
2025-05-18 04:24:09 +0200

    (#143) updated presidio recognizers and converted algorithms file to pdf

--- backend/main.py
+++ backend/main.py
@@ -34,5 +34,5 @@ if __name__ == "__main__":
         log_level="info",  # Set the log level to "info" for server logs.
         workers=1,  # Run with a single worker process.
         limit_concurrency=100,  # Limit the number of concurrent requests.
-        timeout_keep_alive=600,  # Set the keep-alive timeout for connections (in seconds).
+        timeout_keep_alive=1200,  # Set the keep-alive timeout for connections (in seconds).
     )
